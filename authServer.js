98cd35754826d7ddbb05a6ecb7519889d2276ecd
NandeBija <nandebija@gmail.com>
2022-02-17 14:34:15 +0200

    first commit

---
+++ authServer.js
@@ -0,0 +1,48 @@
+// THis server is used for authentication only... only for logins, logouts and refresh tokens //
+
+require("dotenv").config();
+
+const express = require("express");
+const jsonwebtoken = require("jsonwebtoken");
+const app = express();
+
+const jwt = require("jsonwebtoken");
+const posts = [];
+
+app.use(express.json());
+
+let refreshTokens = [];
+
+app.post("/token", (req, res) => {
+  const refreshToken = req.body.token;
+  if (refreshToken == null) return refreshToken.sendStatus(401);
+  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    const accessToken = generateAccessToken({ name: user.name });
+    res.json({ accessToken: accessToken });
+  });
+});
+
+app.delete("/logout", (req, res) => {
+  refreshTokens = refreshTokens.filter((token) => token !== req.body.token);
+  res.sendStatus(204);
+});
+
+app.post("/login", (req, res) => {
+  // User authentication password from the authentication//
+
+  const username = req.body.username;
+  const user = { name: username };
+
+  const accessToken = generateAccessToken(user);
+  const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+  refreshTokens.push(refreshToken);
+  res.json({ accessToken: accessToken, refreshToken: refreshToken });
+});
+
+function generateAccessToken(user) {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15s" });
+}
+
+app.listen(4000);
